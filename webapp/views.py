ecaabd9ce74844a5b048c8df91b93222c6f48886
umemura14 <junya.u0014@outlook.jp>
2022-01-23 15:10:16 +0900

    first_commit

---
+++ webapp/views.py
@@ -0,0 +1,32 @@
+from re import template
+from django import views
+from django.shortcuts import render
+from numpy import generic
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt["username"] = ""
+        return ctxt
+
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt["time"] = "2021.10/01"
+        ctxt["skills"] = [" Java",
+                          "HTML",
+                          "CSS",
+                          "Python",
+                          "Git",
+                          "Linux"
+                          ]
+        return ctxt
